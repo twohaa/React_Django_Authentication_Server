0c2264aa4528d6aef8d83171e0244c971bec179b
Mahfuz Twoha <mahfuzztwohaa@gmail.com>
2024-02-03 13:12:24 +0600

    modified some files

--- server/urls.py
+++ server/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('Customer.urls')),
+    path('auth/', include('Customer.urls')),
 ]
